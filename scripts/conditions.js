dd97434fe7befb86696a2c3deb2a70d410c544b0
Reysian <71834018+Reysian@users.noreply.github.com>
2025-05-10 15:20:56 -0500

    Implement several style changes

--- scripts/conditions.js
+++ scripts/conditions.js
@@ -4,78 +4,104 @@ const lonField = document.getElementById('longitude');
 const tableBody = document.querySelector("#content");
 const tableHeader = document.querySelector("#header");
 
+// Set values of latitude and longitude when submit button clicked or upon submission (ENTER key).
 submit.onclick = function() {
+
+  // If there is input in the latitude field, update the value of latitude in session storage.
   if (latField.value)
     sessionStorage.setItem('lat', latField.value);
+
+  // If there is input in the longitude field, update the value of longitude in session storage.
   if (lonField.value)
     sessionStorage.setItem('lon', lonField.value);
 };
 
+// Reset values of latitude and longitude to their default values (Berlin), and reload the page.
 reset.onclick = function() {
   sessionStorage.setItem('lat', '52.52')
   sessionStorage.setItem('lon', '13.41')
   location.reload();
 }
 
+// Once the entire HTML file has loaded, begin script.
 document.addEventListener("DOMContentLoaded", () => {
   fetchWeatherData();
 });
   
-async function fetchWeatherData() {
+// Fetch weather (current conditions) data from open-meteo and display it properly onscreen.
+async function fetchWeatherData() { 
   
+  // Get latitude and longitude values from session storage, or set them to their default values (Berlin) if they are not in session storage.
   lat = sessionStorage.getItem('lat') || '52.52';
   lon = sessionStorage.getItem('lon') || '13.41';
 
+  // If invalid coordinates are entered, restore latitude and/or longitude to default values (Berlin).
   if (Math.abs(lat) > 90)
     lat = 52.52;
   if (Math.abs(lon) > 180)
     lon = 13.41;
 
-  tableHeader.innerText += " at " + lat + " and " + lon;
+  // Display coordinates in the header.
+  tableHeader.innerText += " at " + lat + ", " + lon;
 
   try {
+
+    // Get current weather conditions from open-meteo.
     const url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,snowfall,showers,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m";
     const response = await fetch(url);
     const data = await response.json();
-  
+
+    // Store units of measurement in a list and conditions in a map.
     const units = data.current_units;
     const conditions = data.current;
-  
+
+    // For each key-value pair in conditions map, create an entry in the table.
     for (const [key, value] of Object.entries(conditions)) {
 
+      // Create a row.
       const row = document.createElement("tr");
+
+      // Format and record the key (name) of the current weather condition.
       const keyCell = document.createElement("td");
-      
       keyCell.textContent = formatKey(key);
-  
+
+      // Format and record the measurement of the current weather condition, concatenating the unit of measurement.
       const valueCell = document.createElement("td");
       valueCell.textContent = formatValue(value, units[key]);
 
+      // Replace the time condition's unit of measurement (iso8601) with (UTC) to indicate global time.
       if (units[key] === "iso8601")
         valueCell.textContent = formatValue(value, "(UTC)");
 
+      // Convert value of is_day to Yes/No instead of 1/0, and disregard the unit of measurement.
       if (key === "is_day")
         valueCell.textContent = value === 1 ? "Yes" : "No";
 
+      // Translate weather_code's numeric value to a description of the current weather condition, and concatenate the description after the value.
       if (key === "weather_code")
         valueCell.textContent = value + " (" + getCondition(value) + ")";
-  
+
+      // // Populate the row with the formated data, and add it to the table body.
       row.appendChild(keyCell);
       row.appendChild(valueCell);
       tableBody.appendChild(row);
     }
   } catch (error) {
+
+    // Throw a console error and show an error message in the table body if the data cannot be fetched/displayed.
     console.error('Error fetching weather data:', err);
     tableBody.textContent = "Error fetching weather data.";
   }
 }
 
+// Format the condition key by replacing underscores with spaces and capitalizing the first letter of every word.
 function formatKey(key) {
   return key
   .replace(/_/g, ' ')
   .replace(/\b\w/g, char => char.toUpperCase());
 }
 
+// Format the condition value by replacing 'T' with a space (date/time), and concatenate the unit of measurement.
 function formatValue(value, unit) {
   return value
   .toString()
@@ -83,6 +109,7 @@ function formatValue(value, unit) {
   + ' ' + unit;
 }
 
+// Given a weather code, return the corresponding weather condition.
 function getCondition(wmoCode) {
 
   let condition = "";
--- scripts/temperature.js
+++ scripts/temperature.js
@@ -6,46 +6,64 @@ const tableBody = document.querySelector("#content");
 const chartHeader = document.querySelector("#header");
 const ctx = document.querySelector("#chart").getContext("2d");
 
+// Set values of latitude and longitude when submit button clicked or upon submission (ENTER key).
 submit.onclick = function() {
+
+  // If there is input in the latitude field, update the value of latitude in session storage.
   if (latField.value)
     sessionStorage.setItem('lat', latField.value);
+
+  // If there is input in the longitude field, update the value of longitude in session storage.
   if (lonField.value)
     sessionStorage.setItem('lon', lonField.value);
 };
 
+// Reset values of latitude and longitude to their default values (Berlin), and reload the page.
 reset.onclick = function() {
   sessionStorage.setItem('lat', '52.52')
   sessionStorage.setItem('lon', '13.41')
   location.reload();
 }
 
+// Once the entire HTML file has loaded, begin script.
 document.addEventListener("DOMContentLoaded", () => {
   fetchWeatherData();
 });
 
+// Fetch weather (temperature) data from open-meteo and display it properly onscreen.
 async function fetchWeatherData() { 
   
+  // Get latitude and longitude values from session storage, or set them to their default values (Berlin) if they are not in session storage.
   lat = sessionStorage.getItem('lat') || '52.52';
   lon = sessionStorage.getItem('lon') || '13.41';
 
+  // If invalid coordinates are entered, restore latitude and/or longitude to default values (Berlin).
   if (Math.abs(lat) > 90)
     lat = 52.52;
   if (Math.abs(lon) > 180)
     lon = 13.41;
 
-  chartHeader.innerText += " at " + lat + " and " + lon;
+  // Display coordinates in the header.
+  chartHeader.innerText += " at " + lat + ", " + lon;
 
   try {
+
+    // Get 7-day hourly temperature predictions from open-meteo.
     const url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&hourly=temperature_2m";
     const response = await fetch(url);
     const data = await response.json();
 
+    // Store each prediction's time and temperature in two lists.
     const times = data.hourly.time;
     const temperatures = data.hourly.temperature_2m;
 
+    // For times displayed on the chart labels, replace 'T' with a space, and remove the year.
     let chartTimes = times.map(function(x){return x.replace(/T/g, '\n').slice(5)});
+
+    // For times displayed in the table, replace 'T' with a space.
     let tableTimes = times.map(function(x){return x.replace(/T/g, ' ');});
 
+    // Create a line plot that shows the predicted hourly change in temperature over 7 days.
     new Chart(ctx, {
       type: 'line',
       data: {
@@ -61,6 +79,7 @@ async function fetchWeatherData() {
         scales: {
           xAxes: [{
             ticks: {
+              // Limit the visible number of x-axis labels to 7, one for each day.
               maxTicksLimit: 7
             }
           }]
@@ -68,20 +87,28 @@ async function fetchWeatherData() {
       }
     });
 
+    // Populate the table with the hourly temperature predictions.
     for (let i = 0; i < times.length; i++) {
+
+      // Create a row.
       const row = document.createElement("tr");
-  
+
+      // Record the predicted time.
       const timeCell = document.createElement("td");
       timeCell.textContent = tableTimes[i];
-  
+
+      // Record the predicted temperature with unit (degrees celcius) at the corresponding time.
       const tempCell = document.createElement("td");
       tempCell.textContent = temperatures[i] + " °C";
-  
+
+      // Populate the row with the recorded data, and add it to the table body.
       row.appendChild(timeCell);
       row.appendChild(tempCell);
       tableBody.appendChild(row);
     }
   } catch (error) {
+
+    // Throw a console error and show an error message in the table body if the data cannot be fetched/displayed.
     console.error("Error fetching weather data:", error);
     tableBody.textContent = "Error fetching weather data.";
   }
